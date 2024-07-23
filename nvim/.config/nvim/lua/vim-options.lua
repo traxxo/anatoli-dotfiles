47f8a1956a101cbf23024430094080a702128a08
traxxo <anischakow@gmail.com>
2024-07-23 19:13:11 +0200

    added nvim module struct

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.cmd([[
+  augroup NumberToggle
+    autocmd!
+    autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
+    autocmd BufLeave,FocusLost,InsertEnter   * set norelativenumber
+  augroup END
+]])
