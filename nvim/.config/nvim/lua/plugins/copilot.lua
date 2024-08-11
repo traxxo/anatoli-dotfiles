3c897f1705a3c56208fe391c62eef6ccf336a323
traxxo <anischakow@gmail.com>
2024-08-11 19:32:38 +0200

    added copilot plugin

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+	"zbirenbaum/copilot.lua",
+	event = "VimEnter",
+	config = function()
+		require("copilot").setup({
+			panel = { enabled = false },
+			suggestion = {
+				auto_trigger = true,
+				keymap = {
+					accept = "<C-l>",
+					next = "<C-]>",
+					prev = "<C-[>",				}
+			}
+		})
+	end,
+    }
