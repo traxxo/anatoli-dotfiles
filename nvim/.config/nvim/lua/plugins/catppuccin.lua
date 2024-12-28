977de68949453cb0942fad7bea273decd9febf83
traxxo <anischakow@gmail.com>
2024-12-28 20:12:35 +0100

    nvim changed latte to mocha

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -7,20 +7,20 @@ return
   lazy = false,  -- Load the plugin immediately
   config = function()
     require("catppuccin").setup({
-      flavour = "latte",  -- Use "latte" as the base flavor
-      color_overrides = {
-        latte = {
-          base = "#1E1E2E",  -- Mocha background (replace latte's background)
-          mantle = "#1E1E2E", -- Mantle is usually close to the base color
-          crust = "#181825",  -- Crust color
-          text = "#D9E0EE",   -- Mocha's text color
-          subtext1 = "#BAC2DE", -- Mocha's subtext1
-          subtext0 = "#A6ADC8", -- Mocha's subtext0
-          overlay2 = "#C3BAC6", -- Mocha's overlay2
-          overlay1 = "#988BA2", -- Mocha's overlay1
-          overlay0 = "#6E6C7E", -- Mocha's overlay0
-        },
-      },
+      flavour = "mocha",  -- Use "latte" as the base flavor
+      -- color_overrides = {
+        -- latte = {
+          -- base = "#1E1E2E",  -- Mocha background (replace latte's background)
+          -- mantle = "#1E1E2E", -- Mantle is usually close to the base color
+          -- crust = "#181825",  -- Crust color
+          -- text = "#D9E0EE",   -- Mocha's text color
+          -- subtext1 = "#BAC2DE", -- Mocha's subtext1
+          -- subtext0 = "#A6ADC8", -- Mocha's subtext0
+          -- overlay2 = "#C3BAC6", -- Mocha's overlay2
+          -- overlay1 = "#988BA2", -- Mocha's overlay1
+          -- overlay0 = "#6E6C7E", -- Mocha's overlay0
+        -- },
+      --},
     })
     vim.cmd([[colorscheme catppuccin]])
   end,
